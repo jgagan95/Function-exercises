0918bf6e1b90411a4506cb6baaca25d4fca08c1f
Josh <39458297+jgagan95@users.noreply.github.com>
2020-10-12 18:09:06 -0500

    Create last-element.py

---
+++ last-element.py
@@ -0,0 +1,15 @@
+#my solution
+#i keep forgetting you don't need else???
+
+def last_element(my_list):
+    if my_list:
+        return my_list[-1]
+    else:
+        return None
+
+#course solution
+
+def last_element(l):
+    if l:
+        return l[-1]
+    return None
