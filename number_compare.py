89fc65c0869e9eeb558a402a5879b6801c01846e
Josh <39458297+jgagan95@users.noreply.github.com>
2020-10-14 19:08:57 -0500

    Create number_compare.py

---
+++ number_compare.py
@@ -0,0 +1,16 @@
+#my solution
+def number_compare(num1, num2):
+    if num1 > num2:
+        return "First is greater"
+    elif num2 > num1:
+        return "Second is greater"
+    return "Numbers are equal"
+    
+#course solution
+
+def number_compare(a,b):
+    if a > b:
+        return "First is greater"
+    elif b > a:
+        return "Second is greater"
+    return "Numbers are equal"
